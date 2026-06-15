8be6d82b1e7b2c621d65e008ab47679bf460736b
laljidhrangiya <dhrangiyadev242@gmail.com>
2026-06-15 11:00:56 +0530

    Add files via upload

---
+++ nodejs/database/update_demo.js
@@ -0,0 +1,10 @@
+var connection = require('./connection');
+var sql = "update category set name='egf',detail ='efg detail', photo = 'no photo.jpg' where =1";
+connection.con.query(sql, function(error,result){
+    if(error !=null){
+        console.log(error);
+    }
+    else{
+        console.log('update category succefulley');
+    }
+})
