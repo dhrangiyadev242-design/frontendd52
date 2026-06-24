05c86389998b635134d860f0f3bf1f27ca3fc9bb
laljidhrangiya <dhrangiyadev242@gmail.com>
2026-06-24 12:52:23 +0530

    Update api.js

--- nodejs/express/api.js
+++ nodejs/express/api.js
@@ -11,7 +11,7 @@ let list = [
     { email: "manas@gmail.com", passsword: "123436" },
 ]
 
-app.get("/list", async (request, response) => {
+app.get("/list", (request, response) => {
     response.json(list);
 });
 app.post("/list", (request, response) => {
