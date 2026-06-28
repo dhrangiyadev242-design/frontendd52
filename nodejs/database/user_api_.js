fdac0b630494f35d5e2c50864e1f84776b68e0ae
laljidhrangiya <dhrangiyadev242@gmail.com>
2026-06-29 00:00:51 +0530

    Add files via upload

---
+++ nodejs/database/user_api_.js
@@ -0,0 +1,159 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connection = require('./connection');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//api of users Register
+app.post("/users/register", (request, response) => {
+    let name = request.body.name;
+    let email = request.body.email;
+    let mobile = request.body.mobile;
+    let password = request.body.password;
+    //response.json({error:'API working'});
+    if (!name || !email || !mobile || !password) {
+        return response.json({ error: 'All Fileds Are Require' });
+    }
+    else {
+        let sql = 'select * from users where email=?';
+        connection.con.query(sql, [email], function (error, result) {
+            if (error) {
+                return response.json({ error: 'oops something wrong' });
+            }
+            if (result.length > 0) {
+                return response.json({
+                    error: 'Email already registered'
+                })
+            }
+            else {
+                let insertSql =
+                    "INSERT INTO users(name,email,mobile,password) VALUES(?,?,?,?)";
+                connection.con.query(
+                    insertSql,
+                    [name, email, mobile, password],
+                    function (error, result) {
+                        if (error) {
+                            return response.json({
+                                error: error.message
+                            });
+                        }
+                        response.json({
+                            success: true,
+                            message: "User Registered Successfully",
+                            id: result.insertId
+                        });
+
+                    }
+                );
+            }
+        });
+    }
+});
+ //Api of users Login
+   app.post("/users/login",(request,response)=>{
+    //Receive Data
+    let email = request.body.email;
+    let password = request.body.password;
+    //validation
+    if(!email || !password){
+        return response.json({error:'Email and Password are required'});
+    }
+    else{
+        //SQL Query
+        let sql = 'select * from users where email=?';
+        //Execute Query
+        connection.con.query(sql,[email],function(error,result){
+            //Database Error
+            if(error){
+                return response.json({error:error.message});
+            }
+            //Email Check
+            if(result.length == 0){
+                return response.json({error:'invalid email'});
+            }
+            //User Object
+            let user = result[0];
+            //Password Check
+            if(user.password !=password){
+                  return response.json({error:'Invalid Password'});
+            }
+            //Success
+                 response.json({success:true,message:'login successfully',user:{
+                    id:user.id,
+                    name:user.name,
+                    email:user.email,
+                    mobile:user.mobile
+                 }});
+        });
+    }
+   });
+   app.post("/users/change_Password",(request,response)=>{
+    //Recive data
+    let email = request.body.email;
+    let old_Password = request.body.old_Password;
+    let new_Password = request.body.new_Password;
+     //validation
+     if(!email || !old_Password || !new_Password){
+        return response.json({error:'All Fields Are Require'});
+     }
+     else{
+        let sql = 'select * from users where email=?';
+        connection.con.query(sql,[email],function(error,result){
+            if(error){
+                return response.json({error:'database error'});
+            }
+            else{
+                if(result.length == 0){
+                    return response.json({error:'user not found'});
+                }
+                let user = result[0];
+                if(user.password !=old_Password){
+                   return response.json({error:'old password is incorrect'});
+                }
+                let sql = 'update users set password=? where email=?';
+                connection.con.query(sql,[new_Password,email],function(error,result){
+                    if(error){
+                        return response.json({error:'password update failed'});
+                    }
+                 response.json({success:true,
+                    message:'password updated successfulley'});
+                });
+            }
+        });
+     }
+   });
+   app.post("/users/forgot_Password",(reqest,response)=>{
+     //receive data
+     let email = reqest.body.email;
+     let new_Password = reqest.body.new_Password;
+     //validation 
+     if(!email || !new_Password){
+        return response.json({error:'All Fields Are Require '}); 
+     }
+     else{
+        let sql = 'select * from  users where email=?';
+        connection.con.query(sql,[email],function(error,result){
+            if(error){
+                return response.json({error:'database error'});
+            }
+            if(result.length == 0){
+                return response.json({error:'Email Not Registerd'});
+            }
+            let sql = 'update users set Password=? where email=?';
+            connection.con.query(sql,[new_Password,email],function(error,result){
+                if(error){
+                    return response.json({error:'new_Password update is failed'});
+                }
+                response.json({success:true,
+                    message:'Password Reset Successfully'
+                });
+            })
+        });
+     }
+   });
+
+app.listen(5000);
+console.log('ready to accept request');
+
+
